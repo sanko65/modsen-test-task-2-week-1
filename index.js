7dc8a74f5f209f50a2b95fc249164a9e8e45190d
sanko <dsanko35@gmail.com>
2024-02-13 22:44:27 +0300

    docs: add Swagger documentation

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
 const meetupRouter = require("./routes/meetupRoutes");
 
 const app = express();
 const PORT = parseInt(process.env.PORT) || 3000;
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Meetup API",
+      version: "0.1.0",
+    },
+    servers: [
+      {
+        url: `http://127.0.0.1:${PORT}/api`,
+      },
+    ],
+  },
+  apis: ["./docs/swagger.yaml"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 app.use(express.json());
 app.use("/api", meetupRouter);
--- validators/meetupValidator.js
+++ validators/meetupValidator.js
@@ -13,7 +13,7 @@ const validateCreateMeetup = (meetupInfo) => {
     name: Joi.string().min(5).max(80).required(),
     description: Joi.string().min(5).max(300).required(),
     keywords: Joi.array()
-      .items(Joi.string().min(5).max(30).required())
+      .items(Joi.string().min(2).max(30).required())
       .optional(),
     time: Joi.date().greater("now").required(),
     place: Joi.string().min(5).max(50),
@@ -28,7 +28,7 @@ const validateUpdateMeetup = (meetupInfo) => {
     name: Joi.string().min(5).max(80).required(),
     description: Joi.string().min(5).max(300).required(),
     keywords: Joi.array()
-      .items(Joi.string().min(5).max(30).required())
+      .items(Joi.string().min(2).max(30).required())
       .optional(),
     time: Joi.date().greater("now").required(),
     place: Joi.string().min(5).max(50),
