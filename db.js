e9d696cbdba01524ef382aee1408d599f34ef280
sanko <dsanko35@gmail.com>
2024-02-15 15:11:33 +0300

    build: heroku preparation

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
 const Pool = require("pg").Pool;
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.DB_USER}:${
+  process.env.DB_PASSWORD
+}@${process.env.DB_HOST}:${parseInt(process.env.DB_PORT)}/${
+  process.env.DB_DATABASE
+}`;
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT),
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+});
+
+pool.on("connect", () => {
+  console.log("Meetup Database connected successfully!");
 });
 
 module.exports = pool;
